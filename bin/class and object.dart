4d9999d5461d31a89540707ac669f2c28f9f723c
Sreejith-k-c <kcsreejith17120@gmail.com>
2023-06-19 20:53:29 +0530

    initial commit

---
+++ bin/class and object.dart
@@ -0,0 +1,56 @@
+///class creation -
+/*  class className {
+    instance variable
+    constructors
+    user defined functions/bulit in function except main function
+    }
+ */
+class students {
+  /// Instance variable -> globally declared variable -> (declared inside the class outside the functions etc)
+  /// -> may or may not have initial value
+  /// -> instance variables always depends on objects
+  /// -> eg:objectName.instanceVariableName
+  /// ->value can be changed
+  String? name;
+  int? age;
+  String? email;
+  int? phno;
+
+
+static String course = "Flutter";
+}
+void main(){
+  /// object creation ->syntax:ClassName objectName=ClassName();
+  /// (constructor:a function with name same as class name)
+  /// used to create an object
+
+  students st1=students();   /// similar way: var st1=students();
+  print("student 1 details");
+  print("Name=${st1.name="Sreejith"}");
+  print("Age=${st1.age=20}");
+  print("Email=${st1.email="sreejithkcsreejith17120@gmail.com"}");
+  print("Phno=${st1.phno=9073349312}");
+
+  print("--------------------------------");
+
+  students st2=students();
+  print("student 2 details");
+  print("Name=${st2.name="Ameen"}");
+  print("Age=${st2.age=20}");
+  print("Email=${st2.email="Ameenmelaga@gmail.com.com"}");
+  print("Phno=${st2.phno=9071542334}");
+
+  print("--------------------------------");
+
+  students st3=students();
+  print("student 3 details");
+  print("Name=${st3.name="Naruto"}");
+  print("Age=${st3.age=16}");
+  print("Email=${st3.email="uzumakinaruto.com.com"}");
+  print("Phno=${st3.phno=9876543210}");
+
+  print("--------------------------------");
+  print(st1.name);
+  print(st2.name);
+  print(st3.name);
+}
---
+++ bin/football.dart
@@ -0,0 +1,38 @@
+class players {
+  String? name;
+  int? age;
+  String? nation;
+  String? club;
+}
+  void main(){
+
+    players pl1=players();
+    print("player 1 details");
+    print("Name=${pl1.name="Messi"}");
+    print("Age=${pl1.age=36}");
+    print("Nation=${pl1.nation="Argentina"}");
+    print("Club=${pl1.club="Inter Miami"}");
+
+    print("--------------------------------");
+
+    players pl2=players();
+    print("player 2 details");
+    print("Name=${pl2.name="Cristiano Ronaldo"}");
+    print("Age=${pl2.age=38}");
+    print("Nation=${pl2.nation="Portugal"}");
+    print("Club=${pl2.club="Al Nassr"}");
+
+    print("--------------------------------");
+
+    players pl3=players();
+    print("player 3 details");
+    print("Name=${pl3.name="Neymar"}");
+    print("Age=${pl3.age=31}");
+    print("Nation=${pl3.nation="Brazil"}");
+    print("Club=${pl3.club="PSG"}");
+
+    print("--------------------------------");
+    print(pl1.name);
+    print(pl2.name);
+    print(pl3.name);
+  }
